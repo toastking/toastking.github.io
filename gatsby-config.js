de6c9e36a319c642aa25220dbff09644ca9d53a1
Matt Del Signore <mattdelsig@me.com>
2018-12-25 22:41:29 -0500

    added progressive web app helpers and manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,17 +26,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Matt Del Signore's blog`,
+        short_name: `mattdelsig`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    'gatsby-plugin-offline',
   ],
 }
