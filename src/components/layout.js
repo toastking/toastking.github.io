0014bbb1ddcd9077194e7b740f89348efa9b54ab
Matt Del Signore <mattdelsig@me.com>
2018-12-28 16:57:57 -0500

    added stuff to deploy

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,6 @@ import { StaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components';
 
 import Header from './header'
-import './layout.css'
 
 const Container = styled.div`
   height:100vh;
